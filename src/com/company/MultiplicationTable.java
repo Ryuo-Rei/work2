ea0ef780ca74c128ea3bc8931ce4aa18b73de0d7
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-10 13:17:53 +0900

    九九 修正

--- src/com/company/MultiplicationTable.java
+++ src/com/company/MultiplicationTable.java
@@ -6,6 +6,7 @@ public class MultiplicationTable {
     public void ShowMultiplicationTable() {
         System.out.println("   1  2  3  *  5  6  7  8  9");
         for(int i = 1; i <= 9; i++) {
+            // 4の場合、*に変換
             if(i == 4) {
                 System.out.print("* ");
             }
@@ -14,6 +15,7 @@ public class MultiplicationTable {
             }
             for(int j = 1; j <= 9; j++) {
                 if(j == 9) {
+                    // 掛けた結果が一桁の場合、インデント合わせのために空白を追加
                     if(i * j < 10) {
                         System.out.println(" " + i * j);
                     }
@@ -25,6 +27,7 @@ public class MultiplicationTable {
                     if(i * j == 4) {
                         System.out.print(" * ");
                     }
+                    // 掛けた結果が一桁の場合、インデント合わせのために空白を追加
                     else if(i * j < 10) {
                         System.out.print(" " + i * j + " ");
                     }
