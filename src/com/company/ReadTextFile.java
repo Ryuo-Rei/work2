765e804e2b2f8aa5543fd5adb38c3cfd8509029b
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-10 14:19:39 +0900

    テキストファイルの読み込み 追加

---
+++ src/com/company/ReadTextFile.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+// 2020/09/10 13:20 ~ 13:35
+public class ReadTextFile {
+
+    final static String FILE_PATH = "C:\\Users\\ryuorei\\Desktop\\Git\\work2\\ReadTextFile用テキスト.txt";
+
+    public void Execute() {
+        List<String> fileContentList = ReadFile(FILE_PATH);
+
+        System.out.println(fileContentList);
+    }
+
+    private List<String> ReadFile(String path) {
+        Path file = Paths.get(path);
+        List<String> fileContentList = null;
+        try {
+            fileContentList = Files.readAllLines(file);
+        } catch (IOException e) {
+            System.out.println("ファイル読込みに失敗しました。");
+        }
+
+        return fileContentList;
+    }
+}
---
+++ src/com/company/ReadTextFileMain.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ReadTextFileMain {
+
+    public static void main(String[] args) {
+        // write your code here
+        ReadTextFile rtf = new ReadTextFile();
+        rtf.Execute();
+    }
+}
